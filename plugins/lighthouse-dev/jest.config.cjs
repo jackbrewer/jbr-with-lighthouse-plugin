5ebc0d03ba70af8a0c2b12112f773bee1dc98469
Jack Brewer <jack@jackbrewer.co.uk>
2023-02-14 11:07:46 +0000

    Remove local plugin

--- plugins/lighthouse-dev/jest.config.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  roots: ['<rootDir>/src'],
-  testMatch: ['**/__tests__/**/*.+(ts|js)', '**/?(*.)+(spec|test).+(ts|js)'],
-  setupFiles: ['<rootDir>/test/setup.js'],
-  transform: {},
-  collectCoverageFrom: ['**/*.js'],
-};
